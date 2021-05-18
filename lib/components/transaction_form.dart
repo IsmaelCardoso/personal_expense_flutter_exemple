85be00d85d1c9ebdc3709edb7d99c27d1d6a5faa
IsmaelCardoso <ismael.cardosos@gmail.com>
2021-05-18 15:55:07 -0300

    chore(main): correction in form for statefull

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
+
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
 
   _submitForm() {
     final title = titleController.text;
     final value = double.tryParse(valueController.text) ?? 0.0;
 
     if (title.isNotEmpty || value > 0) {
-      onSubmit(
+      widget.onSubmit(
         title,
         value,
       );
